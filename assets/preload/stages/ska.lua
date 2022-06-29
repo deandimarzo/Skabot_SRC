f96bf5ca6890999120ea7ba3bf8174347c9d1c15
deandimarzo <deandimarzo@gmail.com>
2022-06-29 13:06:02 -0400

    Initial commit

---
+++ assets/preload/stages/ska.lua
@@ -0,0 +1,22 @@
+function onCreate()
+	-- background 
+
+	makeLuaSprite('floor', 'floor', 0, 0);
+	setScrollFactor('floor', 1, 1);
+    scaleObject('floor', 1.2, 1.2);
+    
+    makeLuaSprite('tables', 'tables', 360, 1200);
+	setScrollFactor('tables', 1.2, 1.2);
+    scaleObject('floor', 1.5, 1.5);
+    
+    setProperty('gfGroup.visible',false);
+    
+
+    
+    
+
+	addLuaSprite('floor', false);
+    addLuaSprite('tables', true);
+
+    
+end
